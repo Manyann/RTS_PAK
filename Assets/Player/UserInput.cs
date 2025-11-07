8f6fe5e41a92062d8c5a31669c313694a1a7505d
MANTELLI Yann <yann.mantelli@soprasteria.com>
2025-11-07 17:01:39 +0100

    feat : ajout commentaire pour lisibilité

--- Assets/Player/UserInput.cs
+++ Assets/Player/UserInput.cs
@@ -14,43 +14,47 @@ public class UserInput : MonoBehaviour {
  
     // Update is called once per frame
     void Update () {
-            MoveCamera();
-            RotateCamera();
+        MoveCamera();
+        RotateCamera();
     }
  
     private void MoveCamera() {
-        float xpos = Input.mousePosition.x;
-        float ypos = Input.mousePosition.y;
+        float positionX = Input.mousePosition.x;
+        float positionY = Input.mousePosition.y;
         Vector3 movement = new Vector3(0,0,0);
  
-        if(xpos >= 0 && xpos < ResourceManager.ScrollWidth) {
+        // Horizontal
+        if(positionX >= 0 && positionX < ResourceManager.ScrollWidth) {
             movement.x -= ResourceManager.ScrollSpeed;
-        } else if(xpos <= Screen.width && xpos > Screen.width - ResourceManager.ScrollWidth) {
+        } else if(positionX <= Screen.width && positionX > Screen.width - ResourceManager.ScrollWidth) {
             movement.x += ResourceManager.ScrollSpeed;
         }
- 
-        if(ypos >= 0 && ypos < ResourceManager.ScrollWidth) {
+        // Vertical
+        if(positionY >= 0 && positionY < ResourceManager.ScrollWidth) {
             movement.z -= ResourceManager.ScrollSpeed;
-        } else if(ypos <= Screen.height && ypos > Screen.height - ResourceManager.ScrollWidth) {
+        } else if(positionY <= Screen.height && positionY > Screen.height - ResourceManager.ScrollWidth) {
             movement.z += ResourceManager.ScrollSpeed;
         }
  
+        // Gestion du zoom
         movement = Camera.main.transform.TransformDirection(movement);
         movement.y = 0;
         movement.y -= ResourceManager.ScrollSpeed * Input.GetAxis("Mouse ScrollWheel");
  
+        // Affectation de la nouvelle position
         Vector3 origin = Camera.main.transform.position;
         Vector3 destination = origin;
         destination.x += movement.x;
         destination.y += movement.y;
         destination.z += movement.z;
  
+        // Zoom dans les max ranges
         if(destination.y > ResourceManager.MaxCameraHeight) {
             destination.y = ResourceManager.MaxCameraHeight;
         } else if(destination.y < ResourceManager.MinCameraHeight) {
             destination.y = ResourceManager.MinCameraHeight;
         }
- 
+
         if(destination != origin) {
             Camera.main.transform.position = Vector3.MoveTowards(origin, destination, Time.deltaTime * ResourceManager.ScrollSpeed);
         }
@@ -60,6 +64,7 @@ public class UserInput : MonoBehaviour {
         Vector3 origin = Camera.main.transform.eulerAngles;
         Vector3 destination = origin;
         
+        // Gestion de ALT appuyé pour trigger la rotation avec combo click droit
         if((Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)) && Input.GetMouseButton(1)) {
             destination.x -= Input.GetAxis("Mouse Y") * ResourceManager.RotateAmount;
             destination.y += Input.GetAxis("Mouse X") * ResourceManager.RotateAmount;
